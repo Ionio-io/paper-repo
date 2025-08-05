9c8a46a0dd911b552db1a071582124dc53b74d67
manideep896 <manideep@ionio.io>
2025-08-05 18:31:07 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,32 @@
+// export const SITE = {
+//   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+//   author: "Sat Naing",
+//   profile: "https://satnaing.dev/",
+//   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+//   title: "AstroPaper",
+//   ogImage: "astropaper-og.jpg",
+//   lightAndDarkMode: true,
+//   postPerIndex: 4,
+//   postPerPage: 4,
+//   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+//   showArchives: true,
+//   showBackButton: true, // show back button in post detail
+//   editPost: {
+//     enabled: true,
+//     text: "Edit page",
+//     url: "https://github.com/satnaing/astro-paper/edit/main/",
+//   },
+//   dynamicOgImage: true,
+//   dir: "ltr", // "rtl" | "auto"
+//   lang: "en", // html lang code. Set this empty and default will be "en"
+//   timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+// } as const;
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://www.ionio.ai/", // replace this with your deployed domain
+  author: "Ionio",
+  profile: "https://www.ionio.ai/",
+  desc: "The Research Hub of Ionio",
+  title: "Ionio",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -11,11 +34,11 @@ export const SITE = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
-  editPost: {
-    enabled: true,
-    text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
-  },
+  // editPost: {
+  //   enabled: true,
+  //   text: "Edit page",
+  //   url: "https://github.com/satnaing/astro-paper/edit/main/",
+  // },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
