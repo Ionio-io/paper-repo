4db57a3e5f93885af05fe1b4a5ef939fcf77e1ac
manideep896 <manideep@ionio.io>
2025-08-14 22:27:07 +0530

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,6 +9,8 @@ const blog = defineCollection({
   schema: ({ image }) =>
     z.object({
       author: z.string().default(SITE.author),
+      writer: z.string().optional(),   // NEW
+      editor: z.string().optional(),   // NEW
       pubDatetime: z.date(),
       modDatetime: z.date().optional().nullable(),
       title: z.string(),
